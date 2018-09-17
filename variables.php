3193235d0c619af46859670b14c40e1ab18d97fa
jose arjona <josedarjona@gmail.com>
2018-09-17 16:19:33 -0400

    variable structure

---
+++ variables.php
@@ -0,0 +1,36 @@
+<?php 
+//these two are NOT the same
+$item
+$Item
+
+//CAMEL CASE
+$myVariable
+
+//SNAKE CASE
+$this_variable
+
+
+//do not use this
+$this-variable
+
+$product3
+
+
+//try not to use it, because php uses it
+$_book
+
+
+//do not multiple underscore, just 1 at minimum
+$__bookPage
+
+
+
+?>
+
+<?php 
+
+//php.net/manual/en/reserved.php   these are reserved words to not use
+
+
+
+?>
